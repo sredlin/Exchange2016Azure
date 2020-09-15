284adc982d5562484daebfd8e321f3d7a1203a7f
Redlin <sredlin@vater-gruppe.de>
2020-09-15 20:55:30 +0200

    Powershell Abkürzungen ausgeschrieben

--- CSDownloadISO.ps1
+++ CSDownloadISO.ps1
@@ -119,8 +119,8 @@ Get-LECertificates
 DownloadISO
 
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
- iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
- sleep 10
+ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+ Start-Sleep 10
  choco install "netfx-4.8" -y
  choco install vscode -y
  choco install microsoft-edge -y
